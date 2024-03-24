97b53dac9c1f256085e55b63d048f267a4a47cec
Muhammad Iqbal <muhammadiqbalips2@gmail.com>
2024-03-24 12:34:02 +0700

    refactory: page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className="carousel w-full h-full pt-20">
+      <div className="carousel w-full h-auto pt-20">
         <div id="slide1" className="carousel-item relative w-full">
           <Image
             src="/bg-1.jpg"
@@ -106,9 +106,9 @@ export default function Home() {
       <div>
         <div className="carousel w-full h-[55vh]">
           <div id="item1" className="carousel-item w-full">
-            <div className="w-full bg-[url('/city.jpeg')] object-cover relative ">
+            <div className="w-full bg-gradient-to-r from-[#cc0000] to-[#b21f29] object-cover relative ">
               <div className="absolute  w-full h-[55vh] flex flex-col items-center pt-6 px-32">
-                <h1 className="text-red-500 text-3xl">
+                <h1 className="text-white text-3xl">
                   What our customers are saying about us
                 </h1>
                 <p className=" text-center pt-8 text-lg text-white">
@@ -135,9 +135,9 @@ export default function Home() {
             </div>
           </div>
           <div id="item2" className="carousel-item w-full">
-            <div className="w-full bg-[url('/city.jpeg')] object-cover relative ">
+            <div className="w-full bg-gradient-to-r from-[#cc0000] to-[#b21f29] object-cover relative ">
               <div className="absolute  w-full h-[55vh] flex flex-col items-center pt-6 px-32">
-                <h1 className="text-red-500 text-3xl">
+                <h1 className="text-white text-3xl">
                   What our customers are saying about us
                 </h1>
                 <p className=" text-center pt-8 text-lg text-white">
@@ -157,9 +157,9 @@ export default function Home() {
             </div>
           </div>
           <div id="item3" className="carousel-item w-full">
-            <div className="w-full bg-[url('/city.jpeg')] object-cover relative ">
+            <div className="w-full bg-gradient-to-r from-[#cc0000] to-[#b21f29] object-cover relative ">
               <div className="absolute  w-full h-[55vh] flex flex-col items-center pt-6 px-32">
-                <h1 className="text-red-500 text-3xl">
+                <h1 className="text-white text-3xl">
                   What our customers are saying about us
                 </h1>
                 <p className=" text-center pt-8 text-lg text-white">
@@ -198,7 +198,7 @@ export default function Home() {
               <polygon points="328.375 384 332.073 458.999 256.211 406.28 179.924 459.049 183.625 384 151.586 384 146.064 496 182.756 496 256.169 445.22 329.242 496 365.936 496 360.414 384 328.375 384"></polygon>
               <path d="M415.409,154.914l-2.194-48.054L372.7,80.933,346.768,40.414l-48.055-2.2L256,16.093,213.287,38.219l-48.055,2.2L139.3,80.933,98.785,106.86l-2.194,48.054L74.464,197.628l22.127,42.715,2.2,48.053L139.3,314.323l25.928,40.52,48.055,2.195L256,379.164l42.713-22.126,48.055-2.195,25.928-40.52L413.214,288.4l2.195-48.053,22.127-42.715Zm-31.646,76.949L382,270.377l-32.475,20.78-20.78,32.475-38.515,1.76L256,343.125l-34.234-17.733-38.515-1.76-20.78-32.475L130,270.377l-1.759-38.514L110.5,197.628,128.237,163.4,130,124.88,162.471,104.1l20.78-32.474,38.515-1.76L256,52.132l34.234,17.733,38.515,1.76,20.78,32.474L382,124.88l1.759,38.515L401.5,197.628Z"></path>
             </svg>
-            <p className="px-6 py-2 text-lg text-black bg-white/70 font-semibold text-center sm:font-bold sm:text-3xl md:text-4xl lg:max-w-2xl xl:max-w-4xl dark:text-gray-700">
+            <p className="px-6 py-2 text-base text-black bg-white/70 font-semibold text-center sm:font-bold sm:text-3xl md:text-4xl lg:max-w-2xl xl:max-w-4xl dark:text-gray-700">
               "As President Director, I'm thrilled to share with you the
               incredible stories and achievements of our company. From
               innovative projects to community initiatives, each page is filled
